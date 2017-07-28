7f9c89878b44a696426dc2908345e058a61b3571
cxwang001 <william@Williams-MBP.attlocal.net>
2017-07-27 19:01:45 -0700

    Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -8,6 +8,16 @@ var connection = mysql.createConnection({
   database: "burger_db"
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'hacktheplanet',
+    database: 'todoagain_db' 
+  });
+};
 // Make connection with db
 connection.connect(function(err) {
   if (err) {
